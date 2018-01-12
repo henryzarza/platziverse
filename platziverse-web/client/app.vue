eebb4dc848c55935f2c97b68fd490594ba7f63ec
Henry Zarza <henryacz7@gmail.com>
2018-01-12 17:35:09 -0500

    Start of the Frontend con Vuejs

---
+++ platziverse-web/client/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <metric uuid="77287010-021c-4618-9a98-21f312d47586" type="promiseMetric"></metric>
+    <agent
+      v-for="agent in agents"
+      :uuid="agent.uuid"
+      :key="agent.uuid">
+    </agent>
+    <p v-if="error">{{error}}</p>
+  </div>
+</template>
+
+<style>
+  body {
+    font-family: Arial;
+    background: #f8f8f8;
+    margin: 0;
+  }
+</style>
+
+<script>
+module.exports = {
+  data () {
+    return {
+      agents: [],
+      error: null
+    }
+  },
+  mounted () {
+    this.initialize()
+  },
+  methods: {
+    initialize () {
+    }
+  }
+}
+</script>
